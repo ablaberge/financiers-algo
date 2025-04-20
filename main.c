04162a52f1ed025241e599d64e5808b276915163
ablaberge <annika.laberge@icloud.com>
2025-04-20 06:02:27 +0000

    Memory leaks plugged :)

--- main.c
+++ main.c
@@ -25,13 +25,13 @@ int main(int argc, char *argv[])
   fscanf(pFile, "%d", &NRES);
   fscanf(pFile, "%d", &NPROC);
 
-  int *available = (int *)malloc(sizeof(int) * NRES);
+  int *total_avail = (int *)malloc(sizeof(int) * NRES);
   for (int i = 0; i < NRES; i++)
   {
-    fscanf(pFile, "%d", &available[i]);
+    fscanf(pFile, "%d", &total_avail[i]);
   }
 
-  int **max_matrix = (int **)malloc(sizeof(int*) * NPROC);
+  int **max_matrix = (int **)malloc(sizeof(int *) * NPROC);
   for (int i = 0; i < NPROC; i++)
   {
     max_matrix[i] = (int *)malloc(sizeof(int) * NRES);
@@ -41,8 +41,8 @@ int main(int argc, char *argv[])
     }
   }
 
-  int **alloc_matrix = (int **)malloc(sizeof(int*) * NPROC);
-  int **need_matrix = (int **)malloc(sizeof(int*) * NPROC);
+  int **alloc_matrix = (int **)malloc(sizeof(int *) * NPROC);
+  int **need_matrix = (int **)malloc(sizeof(int *) * NPROC);
   for (int i = 0; i < NPROC; i++)
   {
     alloc_matrix[i] = (int *)malloc(sizeof(int) * NRES);
@@ -53,11 +53,11 @@ int main(int argc, char *argv[])
       need_matrix[i][j] = max_matrix[i][j] - alloc_matrix[i][j];
     }
   }
-
+  fclose(pFile);
 
   // Sanity check:
-
   int *total_allocated = (int *)malloc(sizeof(int) * NRES);
+  int *available = (int *)malloc(sizeof(int) * NRES);
 
   for (int j = 0; j < NRES; j++)
   {
@@ -69,27 +69,54 @@ int main(int argc, char *argv[])
       {
         printf("Integrity test failed: allocated resources exceed demand for Thread %d. ", i);
         printf("Need %d instances of resource %d. \n", max_matrix[i][j] - alloc_matrix[i][j], j);
+        free(total_allocated);
+        free(available);
+        free(total_avail);
+        for (int i = 0; i < NPROC; i++)
+        {
+          free(max_matrix[i]);
+          free(alloc_matrix[i]);
+          free(need_matrix[i]);
+        }
+
+        free(max_matrix);
+        free(alloc_matrix);
+        free(need_matrix);
+
         return 1;
       }
     }
+    for (int j = 0; j < NRES; j++)
+    {
+      available[j] = total_avail[j] - total_allocated[j];
+    }
 
-    if (total_allocated[j] > available[j])  // Check if total allocated exceeds the total available
+    if (total_allocated[j] > total_avail[j])
     {
       printf("Integrity test failed: allocated resources exceed total resources\n");
+      free(total_allocated);
+      free(available);
+      free(total_avail);
+      for (int i = 0; i < NPROC; i++)
+      {
+        free(max_matrix[i]);
+        free(alloc_matrix[i]);
+        free(need_matrix[i]);
+      }
+
+      free(max_matrix);
+      free(alloc_matrix);
+      free(need_matrix);
       return 1;
     }
   }
+
   free(total_allocated);
-
-
-
-  // TODO: Run banker's safety algorithm
 
   isSafe(available, alloc_matrix, need_matrix, NPROC, NRES);
 
-
-
   free(available);
+  free(total_avail);
   for (int i = 0; i < NPROC; i++)
   {
     free(max_matrix[i]);
