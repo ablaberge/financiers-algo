2f2bbd951d8937cd0baacb402f230cab43ce48cd
ablaberge <annika.laberge@icloud.com>
2025-04-20 00:16:01 +0000

    File handling implemented

--- main.c
+++ main.c
@@ -5,9 +5,57 @@
 
 int main(int argc, char *argv[])
 {
-  // TODO: attempt to open scenario file and scan data into allocated structures
+  if (argc != 2)
+  {
+    printf("Usage: bankers [state file]\n");
+    printf("Please try again using the correct argument\n");
+    return;
+  }
+  // Attempt to open scenario file and scan data into allocated structures
+  FILE *pFile;
+  pFile = fopen(argv[1], "r");
+  if (pFile == NULL)
+  {
+    fclose(pFile);
+    printf("fopen() failed\n");
+    return;
+  }
+  int NRES;  // number of resource types
+  int NPROC; // number of processes
+  fscanf(pFile, "%d", &NRES);
+  fscanf(pFile, "%d", &NPROC);
+
+  int *available = (int *)malloc(sizeof(int) * NRES);
+  for (int i = 0; i < NRES; i++)
+  {
+    fscanf(pFile, "%d", available[i]);
+  }
+
+  int **max_matrix = (int **)malloc(sizeof(int) * NPROC);
+  for (int i = 0; i < NPROC; i++)
+  {
+    max_matrix[i] = (int *)malloc(sizeof(int) * NRES);
+    for (int j = 0; j < NRES; j++)
+    {
+      fscanf(pFile, "%d", max_matrix[i][j]);
+    }
+  }
+
+  int **alloc_matrix = (int **)malloc(sizeof(int) * NPROC);
+  for (int i = 0; i < NPROC; i++)
+  {
+    alloc_matrix[i] = (int *)malloc(sizeof(int) * NRES);
+    for (int j = 0; j < NRES; j++)
+    {
+      fscanf(pFile, "%d", alloc_matrix[i][j]);
+    }
+  }
 
   // TODO: Run banker's safety algorithm
+
+
+
+  // TODO: return malloc'd mem (alloc_matrix, max_matrix, available vector)
 
   return 0;
 }
