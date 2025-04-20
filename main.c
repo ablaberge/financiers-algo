8adc0e545577e80b1ac74108eaa34feb3b89b54d
ablaberge <annika.laberge@icloud.com>
2025-04-20 05:18:01 +0000

    Sanity check code tweaked

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
   {
     printf("Usage: bankers [state file]\n");
     printf("Please try again using the correct argument\n");
-    return;
+    return 1;
   }
   // Attempt to open scenario file and scan data into allocated structures
   FILE *pFile;
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
   {
     fclose(pFile);
     printf("fopen() failed\n");
-    return;
+    return 1;
   }
   int NRES;  // number of resource types
   int NPROC; // number of processes
@@ -28,57 +28,78 @@ int main(int argc, char *argv[])
   int *available = (int *)malloc(sizeof(int) * NRES);
   for (int i = 0; i < NRES; i++)
   {
-    fscanf(pFile, "%d", available[i]);
+    fscanf(pFile, "%d", &available[i]);
   }
 
-  int **max_matrix = (int **)malloc(sizeof(int) * NPROC);
+  int **max_matrix = (int **)malloc(sizeof(int*) * NPROC);
   for (int i = 0; i < NPROC; i++)
   {
     max_matrix[i] = (int *)malloc(sizeof(int) * NRES);
     for (int j = 0; j < NRES; j++)
     {
-      fscanf(pFile, "%d", max_matrix[i][j]);
+      fscanf(pFile, "%d", &max_matrix[i][j]);
     }
   }
 
-  int **alloc_matrix = (int **)malloc(sizeof(int) * NPROC);
+  int **alloc_matrix = (int **)malloc(sizeof(int*) * NPROC);
+  int **need_matrix = (int **)malloc(sizeof(int*) * NPROC);
   for (int i = 0; i < NPROC; i++)
   {
     alloc_matrix[i] = (int *)malloc(sizeof(int) * NRES);
+    need_matrix[i] = (int *)malloc(sizeof(int) * NRES);
     for (int j = 0; j < NRES; j++)
     {
-      fscanf(pFile, "%d", alloc_matrix[i][j]);
+      fscanf(pFile, "%d", &alloc_matrix[i][j]);
+      need_matrix[i][j] = max_matrix[i][j] - alloc_matrix[i][j];
     }
   }
+
 
   // Sanity check:
 
-  for (int i = 0; i < NPROC; i++)
+  int *total_allocated = (int *)malloc(sizeof(int) * NRES);
+
+  for (int j = 0; j < NRES; j++)
   {
-    for (int j = 0; j < NRES; j++)
+    total_allocated[j] = 0;
+    for (int i = 0; i < NPROC; i++)
     {
-      // Ensure that the currently allocated resources do not exceed the total number of resources.
-      if (alloc_matrix[i][j] > available[i])
+      total_allocated[j] += alloc_matrix[i][j];
+      if (alloc_matrix[i][j] > max_matrix[i][j]) // Ensure each thread’s needs do not exceed its max demands for each resource type.
       {
-        printf("Integrity test failed: allocated resources exceed total resources\n");
-        return;
-      }
-      
-      // Ensure each thread’s needs do not exceed its max demands for each resource type.
-      if (alloc_matrix[i][j] > max_matrix[i][j])
-      {
-        printf("Integrity test failed: allocated resources exceed demand for Thread %d", i);
-        printf("Need %d instances of resource %d",max_matrix[i][j] - alloc_matrix[i][j], j);
-        return;
+        printf("Integrity test failed: allocated resources exceed demand for Thread %d. ", i);
+        printf("Need %d instances of resource %d. \n", max_matrix[i][j] - alloc_matrix[i][j], j);
+        return 1;
       }
     }
+
+    if (total_allocated[j] > available[j])  // Check if total allocated exceeds the total available
+    {
+      printf("Integrity test failed: allocated resources exceed total resources\n");
+      return 1;
+    }
   }
+  free(total_allocated);
 
 
 
   // TODO: Run banker's safety algorithm
 
-  // TODO: return malloc'd mem (alloc_matrix, max_matrix, available vector)
+  isSafe(available, alloc_matrix, need_matrix, NPROC, NRES);
+
+
+
+  free(available);
+  for (int i = 0; i < NPROC; i++)
+  {
+    free(max_matrix[i]);
+    free(alloc_matrix[i]);
+    free(need_matrix[i]);
+  }
+
+  free(max_matrix);
+  free(alloc_matrix);
+  free(need_matrix);
 
   return 0;
 }
